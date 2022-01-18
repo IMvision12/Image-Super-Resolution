5dcd3f25fa9448f21c105df391c31080a7d27123
IMvision12 <88665786+IMvision12@users.noreply.github.com>
2022-01-18 18:33:08 +0530

    Update utils.py

--- EDSR/utils.py
+++ EDSR/utils.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 import numpy as np
 
-DIV2K_RGB_MEAN = np.array([0.4488, 0.4371, 0.4040]) * 255
+mean = np.array([0.4488, 0.4371, 0.4040]) * 255
 
-def normalize(x,rgb_mean=DIV2K_RGB_MEAN):
-    return (x - rgb_mean) / 127.5
+def normalize(x,mean):
+    return (x - mean) / 127.5
 
-def denormalize(x,rgb_mean=DIV2K_RGB_MEAN):
-    return (x * 127.5 )+ rgb_mean
+def denormalize(x,mean):
+    return (x * 127.5 )+ mean
 
 def pixel_shuffle(scale):
     return lambda x: tf.nn.depth_to_space(x, scale)
