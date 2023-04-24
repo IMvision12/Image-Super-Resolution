11b241045b61a19fa98749fed32be105e80b8b9c
Vision <gitesh.ch.0912@gmail.com>
2023-04-25 02:10:14 +0530

    Fix

--- models/edsr.py
+++ models/edsr.py
@@ -17,32 +17,20 @@ def Upsampling(inputs, factor=2, **kwargs):
     x = tf.keras.layers.Lambda(pixel_shuffle(scale=factor))(x)
     return x
 
-
-class EDSR(tf.keras.Model):
-    def __init__(self, num_blocks=16, num_filters=64, **kwargs):
-        input_layer = tf.keras.layers.Input(shape=(None, None, 3))
-        x = NormalizeEDSR()(input_layer)
-        x = x_new = tf.keras.layers.Conv2D(num_filters, 3, padding="same")(x)
-
-        for _ in range(num_blocks):
-            x_new = ResBlock(x_new)
-
-        x_new = tf.keras.layers.Conv2D(num_filters, 3, padding="same")(x_new)
-        x = tf.keras.layers.Add()([x, x_new])
-
-        x = Upsampling(x)
-        x = tf.keras.layers.Conv2D(3, 3, padding="same")(x)
-        output_layer = DenormalizeEDSR()(x)
-        super().__init__(inputs=input_layer, outputs=output_layer, **kwargs)
-
-        self.num_blocks = num_blocks
-        self.num_filters = num_filters
-
-    def get_config(self):
-        return {
-            "num_blocks": self.num_blocks,
-            "num_filters": self.num_filters,
-        }
-
 #EDSR baseline model with 16 residual blocks and 64 filters
 #EDSR model with 32 residual blocks and 256 filters
+def EDSR(num_blocks=16, num_filters=64):
+    input_layer = tf.keras.layers.Input(shape=(None, None, 3))
+    x = NormalizeEDSR()(input_layer)
+    x = x_new = tf.keras.layers.Conv2D(num_filters, 3, padding='same')(x)
+
+    for _ in range(num_blocks):
+        x_new = ResBlock(x_new)
+        
+    x_new = tf.keras.layers.Conv2D(num_filters, 3, padding='same')(x_new)
+    x = tf.keras.layers.Add()([x, x_new])
+
+    x = Upsampling(x)
+    x = tf.keras.layers.Conv2D(3, 3, padding='same')(x)
+    output_layer = DenormalizeEDSR()(x)
+    return tf.keras.models.Model(input_layer, output_layer)
--- models/srgan.py
+++ models/srgan.py
@@ -14,35 +14,24 @@ def Discriminator_block(inputs, num_filters, strides=1):
     return x
 
 
-class Discriminator(tf.keras.Model):
-    def __init__(self, num_filters=64, **kwargs):
-        input_layer = tf.keras.layers.Input(shape=(96, 96, 3))
-        x = Normalize2SRGAN()(input_layer)
-        x = tf.keras.layers.Conv2D(
-            num_filters, kernel_size=3, strides=1, padding="same"
-        )(x)
-        x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
+def Discriminator(num_filters=64):
+    input_layer = tf.keras.layers.Input(shape=(96, 96, 3))
+    x = Normalize2SRGAN()(input_layer)
+    x = tf.keras.layers.Conv2D(num_filters, kernel_size=3, strides=1, padding='same')(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
 
-        x = tf.keras.layers.Conv2D(
-            num_filters, kernel_size=3, strides=2, padding="same"
-        )(x)
-        x = tf.keras.layers.BatchNormalization(momentum=0.8)(x)
-        x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
+    x = tf.keras.layers.Conv2D(num_filters, kernel_size=3, strides=2, padding='same')(x)
+    x = tf.keras.layers.BatchNormalization(momentum=0.8)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
 
-        x = Discriminator_block(x, 128)
-        x = Discriminator_block(x, 256)
-        x = Discriminator_block(x, 512)
+    x = Discriminator_block(x, 128)
+    x = Discriminator_block(x, 256)
+    x = Discriminator_block(x, 512)
 
-        x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Flatten()(x)
 
-        x = tf.keras.layers.Dense(1024)(x)
-        x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
-        output_layer = tf.keras.layers.Dense(1, activation="sigmoid")(x)
-        super().__init__(inputs=input_layer, outputs=output_layer, **kwargs)
+    x = tf.keras.layers.Dense(1024)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
+    output_layer = tf.keras.layers.Dense(1, activation='sigmoid')(x)
 
-        self.num_filters = num_filters
-
-    def get_config(self):
-        return {
-            "num_filters": self.num_filters,
-        }
+    return tf.keras.models.Model(input_layer, output_layer)
