7ade51e67afe6da8601cfc165642dab38605384d
IMvision12 <88665786+IMvision12@users.noreply.github.com>
2023-04-25 23:36:18 +0530

    Update edsr.py

--- models/edsr.py
+++ models/edsr.py
@@ -3,17 +3,17 @@ from .utils import NormalizeEDSR, DenormalizeEDSR, pixel_shuffle
 
 
 # ResBlock
-def ResBlock(inputs):
-    x = tf.keras.layers.Conv2D(64, 3, padding="same", activation="relu")(inputs)
-    x = tf.keras.layers.Conv2D(64, 3, padding="same")(x)
+def ResBlock(inputs, num_filters):
+    x = tf.keras.layers.Conv2D(num_filters, 3, padding="same", activation="relu")(inputs)
+    x = tf.keras.layers.Conv2D(num_filters, 3, padding="same")(x)
     x = tf.keras.layers.Add()([inputs, x])
     return x
 
 
-def Upsampling(inputs, factor=2, **kwargs):
-    x = tf.keras.layers.Conv2D(64 * (factor**2), 3, padding="same", **kwargs)(inputs)
+def Upsampling(inputs, num_filters, factor=2, **kwargs):
+    x = tf.keras.layers.Conv2D(num_filters * (factor**2), 3, padding="same", **kwargs)(inputs)
     x = tf.keras.layers.Lambda(pixel_shuffle(scale=factor))(x)
-    x = tf.keras.layers.Conv2D(64 * (factor**2), 3, padding="same", **kwargs)(x)
+    x = tf.keras.layers.Conv2D(num_filters * (factor**2), 3, padding="same", **kwargs)(x)
     x = tf.keras.layers.Lambda(pixel_shuffle(scale=factor))(x)
     return x
 
@@ -26,12 +26,12 @@ def EDSR(num_blocks=16, num_filters=64):
     x = x_new = tf.keras.layers.Conv2D(num_filters, 3, padding="same")(x)
 
     for _ in range(num_blocks):
-        x_new = ResBlock(x_new)
+        x_new = ResBlock(x_new, num_filters)
 
     x_new = tf.keras.layers.Conv2D(num_filters, 3, padding="same")(x_new)
     x = tf.keras.layers.Add()([x, x_new])
 
-    x = Upsampling(x)
+    x = Upsampling(x, num_filters)
     x = tf.keras.layers.Conv2D(3, 3, padding="same")(x)
     output_layer = tf.keras.layers.Lambda(DenormalizeEDSR)(x)
     return tf.keras.models.Model(input_layer, output_layer)
