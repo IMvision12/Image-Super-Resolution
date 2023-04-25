e2545c0afd6b337e6a879f7b7af1007aa76c5fea
Vision <gitesh.ch.0912@gmail.com>
2023-04-25 16:18:29 +0530

    fix

--- models/utils.py
+++ models/utils.py
@@ -1,47 +1,22 @@
 import tensorflow as tf
 import numpy as np
 
-@tf.keras.utils.register_keras_serializable("models")
-class NormalizeEDSR(tf.keras.layers.Layer):
-    def __init__(self):
-        super(NormalizeEDSR, self).__init__()
-        self.mean = np.array([0.4488, 0.4371, 0.4040]) * 255
-        
-    def call(self, inputs):
-        return tf.divide(tf.subtract(inputs, self.mean), 127.5)
+mean = np.array([0.4488, 0.4371, 0.4040]) * 255
 
-@tf.keras.utils.register_keras_serializable("models")
-class DenormalizeEDSR(tf.keras.layers.Layer):
-    def __init__(self):
-        super(DenormalizeEDSR, self).__init__()
-        self.mean = np.array([0.4488, 0.4371, 0.4040]) * 255
-        
-    def call(self, inputs):
-        return tf.add(tf.multiply(inputs, 127.5), self.mean)
+def NormalizeEDSR(x, mean):
+    return (x - mean) / 127.5
 
-@tf.keras.utils.register_keras_serializable("models")
-class NormalizeSRGAN(tf.keras.layers.Layer):
-    def __init__(self):
-        super(NormalizeSRGAN, self).__init__()
-        
-    def call(self, inputs):
-        return tf.divide(inputs, 255.0)
+def DenormalizeEDSR(x, mean):
+    return (x * 127.5 ) + mean
 
-@tf.keras.utils.register_keras_serializable("models")
-class Normalize2SRGAN(tf.keras.layers.Layer):
-    def __init__(self):
-        super(Normalize2SRGAN, self).__init__()
-        
-    def call(self, inputs):
-        return tf.divide(tf.subtract(inputs, 127.5), 127.5)
-    
-@tf.keras.utils.register_keras_serializable("models")
-class Denormalize2SRGAN(tf.keras.layers.Layer):
-    def __init__(self):
-        super(Denormalize2SRGAN, self).__init__()
-        
-    def call(self, inputs):
-        return tf.multiply(tf.add(inputs, 1), 127.5)
+def NormalizeSRGAN(x):
+    return x / 255.0
+
+def Normalize2SRGAN(x):
+    return x / 127.5 - 1
+
+def Denormalize2SRGAN(x):
+    return (x + 1) * 127.5
 
 def pixel_shuffle(scale):
     return lambda x: tf.nn.depth_to_space(x, scale)
